057c6d38f687eb5371d09cdff4ea49613ac80507
phenomenalJM <johntshis@gmail.com>
2021-11-27 00:22:24 +0100

    Beta 2021-11-27 00:22 v1.0.0

---
+++ head.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- mobile metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<!-- site metas -->
+<title>COPTAM&mdash;RDC asbl</title>
+<meta name="description" content="application coptam-RDC page des gestionnaire" />
+<meta name="keywords" content="gestionnaire des rotards, Coptam-RDC" />
+<meta name="coptam-RDC" content="gestionnaire.co" />
+<!-- Favicons -->
+<link href="static/img/index.jpg" rel="icon">
+<link href="static/img/index.jpg" rel="apple-touch-icon">
+<link rel="stylesheet" href="static/vendors/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="style/css/bootstrap-responsive.min.css">
+<link rel="stylesheet" href="static/vendors/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="static/vendors/animate.css/animate.min.css">
+<link rel="stylesheet" href="static/MyFontsWebfontsKit/MyFontsWebfontsKit.css">
+<link rel="stylesheet" href="static/css/style.css">
+<!-- External link -->
+<!-- CSS only -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+<!-- le style de la page de traitement -->
+<link rel="stylesheet" href="static/css/gestion.css">
+<script src="static/vendors/jquery/jquery.min.js"></script>
